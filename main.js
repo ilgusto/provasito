93ca319c5114aeacce29d04069197c6e81f8a46c
ilgusto <devoluca@gmail.com>
2024-06-29 18:19:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+// scripts/main.js
+$(document).ready(function() {
+    $('#hamburger').click(function() {
+        $(this).toggleClass('open');
+        $('#nav-menu').toggleClass('open');
+    });
 
+    // Scroll-triggered animations
+    const sections = document.querySelectorAll('.section');
+
+    const options = {
+        threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__fadeInUp');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
